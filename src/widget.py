bf033ca390f712e00a5cdae7879713a07e1c1035
irina <irina2001f@gmail.com>
2025-02-04 11:33:09 +0300

    file was edited after using mypy and flake8

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,9 @@
-from masks import get_mask_card_number, get_mask_account
+from typing import Any
 
-def mask_account_card(info: str) -> str:
+from masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(info: str) -> Any:
     """Возвращает строку с замаскированным номером"""
     if "Счет" in info:
         return get_mask_account(int(info[5:]))
@@ -8,7 +11,7 @@ def mask_account_card(info: str) -> str:
         return get_mask_card_number(int(info[-16:]))
 
 
-def get_date(date: str) -> str:
+def get_date(date: str) -> Any:
     """возвращает строку с датой в формате ДД.ММ.ГГГГ"""
-    result = date[8:10] + '.' + date[5:7] + '.' + date[:4]
+    result = date[8:10] + "." + date[5:7] + "." + date[:4]
     return result
