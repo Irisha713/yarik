061bfca30648e4488e79dfa8ff21f0d68151ca64
irina <irina2001f@gmail.com>
2025-03-26 14:02:44 +0300

    homework 11.2 is done

---
+++ src/external_api.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+api_key = os.getenv("API_TOKEN")
+
+def get_amount(transaction):
+    """принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
+    amount = float(transaction.get('operationAmount').get('amount'))
+    code = transaction.get('operationAmount').get('currency').get('code')
+
+    if code == 'RUB':
+        return float(amount)
+
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={'RUB'}&from={code}&amount={amount}"
+
+    payload = {}
+    headers = {
+        "apikey": api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    status_code = response.status_code
+    if status_code == 200:
+        result = response.json()
+        return result['result']
+    else:
+        return []
