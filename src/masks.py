e927b4c0570f5730bde9b15c7f6ad537aa6e59bd
irina <irina2001f@gmail.com>
2025-03-31 15:12:56 +0300

    homework 12.2 is done

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,28 @@
+import logging
+
+root_logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("../logs/masks.log")
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+root_logger.addHandler(file_handler)
+root_logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(number_card: int) -> str:
     """Выводит замаскированный номер карты пользователя"""
     card_number = str(number_card)
     if card_number == '':
+        root_logger.error("отсутствует номер карты")
         return ''
     elif card_number.isdigit() and len(card_number) == 16:
         groups_of_card_number = [card_number[:4], card_number[4:6] + "**", "****", card_number[-4:]]
         result = ""
         for i in groups_of_card_number:
             result = result + i + " "
+        root_logger.info("Выведен замаскированный номер карты")
         return result[:-1]
     else:
+        root_logger.error("Некорректный ввод")
         return 'Некорректный ввод'
 
 
